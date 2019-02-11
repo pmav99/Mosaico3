b17db077cdfae4f6630dba7d62202e3e85418a1b
Panos Mavrogiorgos <pmav99@gmail.com>
2019-02-11 21:03:41 +0200

    [lint] Use whitecloud's ansible style
    https://github.com/whitecloud/ansible-styleguide

--- servers/kvm/setup.sh
+++ servers/kvm/setup.sh
@@ -5,11 +5,11 @@ cd "$(dirname $0)"
 KEYPUB="$(cat $HOME/.ssh/id_ecdsa.pub)"
 # the passwd is the first 16 chars of the sha256sum of the private key
 # encoded with md5sum to put in the configuration file
-PASSWD="$(cat /home/msciab/.ssh/id_ecdsa | sha256sum | head -c 16)"
-mkdir -p /var/kvm/mosaico 
+PASSWD="$(cat $HOME/.ssh/id_ecdsa | sha256sum | head -c 16)"
+mkdir -p /var/kvm/mosaico
 virsh net-create network.xml
-for i in 0 1 2 3 
-do 
+for i in 0 1 2 3
+do
     if test "$i" = "0"
     then HOST="master"
     else HOST="node$i"
